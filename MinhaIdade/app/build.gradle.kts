c2cb8ea1a9548baa7c9239d3c527c0bea37241f2
Marcelo Velasque <perfil3.0@hotmail.com>
2025-03-01 10:16:38 -0300

    Corrigido submódulo live-android-18-02-2025

--- MinhaIdade/app/build.gradle.kts
+++ MinhaIdade/app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.media3.common.ktx)
+    implementation(libs.firebase.crashlytics.buildtools)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- MinhaIdade/app/src/main/java/br/com/fiap/minhaidade/MainActivity.kt
+++ MinhaIdade/app/src/main/java/br/com/fiap/minhaidade/MainActivity.kt
@@ -4,19 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.media3.common.util.Log
 import br.com.fiap.minhaidade.ui.theme.MinhaIdadeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,14 +57,51 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MeuComponente() {
-    Column(){
-        Text(text = "Qual sua idadde?")
-        Text(text = "Pressione os Botões para informar sua idade")
-        Row() {
-            Button(onClick = {}) {
+
+    var idade = remember {
+        mutableStateOf(value = 0)
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
+
+        Text(text = "Qual sua idade?", color = Color.Red, fontSize = 24.sp,
+            fontWeight = FontWeight.Bold, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth() )
+
+        Text(text = "Pressione os Botões para informar sua idade",
+            fontSize = 12.sp,
+            color = Color.Blue,
+            textAlign = TextAlign.Center,
+           // modifier = Modifier.fillMaxWidth()
+            )
+        Spacer(modifier = Modifier.height(25.dp))
+        Text(
+            text = "${idade.value}", fontSize = 48.sp,
+            color = Color.Red,
+            textAlign = TextAlign.Center,
+            //modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Row(horizontalArrangement = Arrangement.Center
+            ,modifier = Modifier.fillMaxWidth()) {
+
+            Button(onClick = {
+                idade.value--
+                Log.i("FIAP","MeuComponente ${idade.value}")}
+            ,
+                modifier = Modifier.size(84.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(color = 0xFF88c6a5)),
+                shape = RectangleShape
+            ){
                 Text(text = "-")
             }
-            Button(onClick = {}) {
+            Spacer(modifier = Modifier.width(25.dp))
+            Button(onClick = { idade.value++
+                             Log.i("FIAP","MeuComponente ${idade.value}")},
+                modifier = Modifier.size(84.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(color = 0xFF88c6a5)),
+                shape = RectangleShape) {
                 Text(text = "+")
             }
         }
