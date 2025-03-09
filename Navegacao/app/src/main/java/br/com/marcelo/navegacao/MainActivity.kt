ccd92401bca6404ca5c756cf8aad50c815cf7b61
Marcelo Velasque <perfil3.0@hotmail.com>
2025-03-09 16:06:20 -0300

    atividade de pratica

---
+++ Navegacao/app/src/main/java/br/com/marcelo/navegacao/MainActivity.kt
@@ -0,0 +1,55 @@
+package br.com.marcelo.navegacao
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.marcelo.navegacao.screens.LoginScreen
+import br.com.marcelo.navegacao.screens.MenuScreen
+import br.com.marcelo.navegacao.screens.PedidosScreen
+import br.com.marcelo.navegacao.screens.PerfilScreen
+import br.com.marcelo.navegacao.ui.theme.NavegaçãoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavegaçãoTheme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController, startDestination = "login"
+                    ) {
+                        composable(route = "login") { LoginScreen(navController) }
+
+                        composable(route = "menu") { MenuScreen(
+                            navController = TODO()
+                        ) }
+
+                        composable(route = "pedidos") { PedidosScreen(
+                            navController = TODO()
+                        ) }
+
+                        composable(route = "perfil") { PerfilScreen(
+                            navController = TODO()
+                        ) }
+                    }
+
+                }
+
+                }
+            }
+        }
+    }
+
+
