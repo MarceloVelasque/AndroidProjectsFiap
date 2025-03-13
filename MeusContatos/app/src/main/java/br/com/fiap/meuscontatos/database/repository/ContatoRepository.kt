cdbe7e829c6821cb6c2aca91c7c651a08e49d7ed
Marcelo Velasque <perfil3.0@hotmail.com>
2025-03-13 09:26:10 -0300

    implementação do ContatoDao, contatoDb, e o ContatoRepository, sigo fazendo a implementação

---
+++ MeusContatos/app/src/main/java/br/com/fiap/meuscontatos/database/repository/ContatoRepository.kt
@@ -0,0 +1,31 @@
+package br.com.fiap.meuscontatos.database.repository
+
+import android.content.Context
+import br.com.fiap.meuscontatos.database.dao.ContatoDb
+import br.com.fiap.meuscontatos.model.Contato
+
+class ContatoRepository(context: Context) {
+
+    var db = ContatoDb.getDatabase(context).contatoDao()
+
+    fun salvar(contato: Contato): Long {
+        return db.salvar(contato = contato)
+    }
+
+    fun atualizar(contato: Contato): Int{
+        return db.atualizar(contato = contato)
+    }
+
+    fun excluir(contato: Contato): Int {
+        return db.excluir(contato = contato)
+    }
+
+    fun buscarContatoPeloId(id: Long): Contato{
+        return db.buscarContatoPeloId(id=id)
+    }
+
+    fun istarContato(): List<Contato>{
+        return db.listarContatos()
+    }
+
+}
